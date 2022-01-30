71ed11770d2339592bb4ce5bc66a2709b0374e88
maruf <mmaruf1137@gmail.com>
2022-01-30 13:30:11 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 
 $('.owl-carousel').owlCarousel({
-  loop:true,
+  loop:false,
   margin:0,
   nav:true,
+  navText: [`<svg><use xlink:href="img/icons.svg#icon-chevron-thin-left"></use></svg>`,`<svg><use xlink:href="img/icons.svg#icon-chevron-thin-right"></use></svg>`],
+  // navText: [`<i class="fas fa-address-card"></i>`,`next`],
   responsive:{
       0:{
           items:1
@@ -13,10 +15,9 @@ $('.owl-carousel').owlCarousel({
 const owlDots = document.querySelector(".owl-dots")
 
 function func() {
-  const arr = ['home', 'about us', 'partner', 'portfolio', 'contact us', ]
+  const arr = ['home', 'about us', 'portfolio', 'partner', 'contact us', ]
   const markup = arr.map(el=>{
-      console.log(el);
-      return `<button role="button" class="owl-dot">${el}</button>`
+      return `<button role="button" class='${el === 'home' ? "owl-dot active" : 'owl-dot'}'>${el}</button>`
     }).join('')
   
   owlDots.innerHTML = '';
