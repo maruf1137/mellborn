f650a6d2a5953df77efe4e2af563005439f0fba8
maruf <mmaruf1137@gmail.com>
2022-01-31 09:21:06 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -13,15 +13,33 @@ $('.owl-carousel').owlCarousel({
 })
 
 const owlDots = document.querySelector(".owl-dots")
+const bars = document.querySelector(".nav__bars")
 
-function func() {
+function func(parentEl) {
   const arr = ['home', 'about us', 'portfolio', 'partner', 'contact us', ]
   const markup = arr.map(el=>{
       return `<button role="button" class='${el === 'home' ? "owl-dot active" : 'owl-dot'}'>${el}</button>`
     }).join('')
   
-  owlDots.innerHTML = '';
-  owlDots.insertAdjacentHTML('afterbegin', markup)
+    parentEl.innerHTML = '';
+    parentEl.insertAdjacentHTML('afterbegin', markup)
+}
+func(owlDots)
+
+function mblNav() {
+  const mblBar = document.querySelector('.mbl--nav')
+  console.log(mblBar);
+  if(bars.classList.contains('active')){
+    mblBar.classList.add('nav-active')
+    console.log(mblBar.classList);
+    func(mblBar)
+  }else{
+    mblBar.classList.remove('nav-active')
+  }
 }
 
-func()
+bars.addEventListener('click', ()=>{
+  bars.classList.toggle('active')
+  
+  mblNav()
+})
